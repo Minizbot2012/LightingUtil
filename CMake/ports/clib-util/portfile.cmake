4b8f3a8cc1ed63a9bf378888bedfd33bad885217
Mini <mini@minizbot.org>
2026-01-05 12:44:53 -0500

    Fix clibutil

--- CMake/ports/clib-util/portfile.cmake
+++ CMake/ports/clib-util/portfile.cmake
@@ -1,7 +1,7 @@
 # header-only library
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL https://github.com/powerof3/CommonLibSSE
+    URL https://github.com/powerof3/CLibUtil
     HEAD_REF master
     REF 789abd27b8955e974da53713898bf8a3db0e2a66
 )
